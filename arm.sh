57c60c37fadbc07d8d9e7cb4b111676a85dade01
YuanTianhang <42244015+yuant2007@users.noreply.github.com>
2022-02-04 17:11:30 +0800

    Update arm.sh

--- arm.sh
+++ arm.sh
@@ -1,1 +1,32 @@
-echo test
+cat > /etc/sysctl.conf <<EOF
+fs.file-max = 1024000
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.core.rmem_default = 65536
+net.core.wmem_default = 65536
+net.core.netdev_max_backlog = 4096
+net.core.somaxconn = 4096
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.ip_local_port_range = 10000 65000
+net.ipv4.tcp_max_syn_backlog = 4096
+net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_rmem = 4096 65536 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.ip_forward = 1
+net.ipv4.tcp_fastopen = 3
+net.core.default_qdisc = fq
+net.ipv4.tcp_congestion_control = bbr
+EOF
+sysctl -p && sysctl --system
+
+wget https://github.com/nkeonkeo/neko-relay-land/releases/latest/download/neko-relay_linux_arm64 -O /usr/bin/neko-relay
+chmod +x /usr/bin/neko-relay
+neko-relay -g init
+
+wget https://github.com/yuant2007/tunnel_config/raw/main/neko_config.yaml -O /etc/neko-relay/config.yaml
+
+neko-relay restart
